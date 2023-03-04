a6442f8afc08ad34079400d4cb59b6244aaf0577
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2023-03-04 12:02:30 +0100

    Completed Dashboard Homepage

--- src/components/Home/Dashboard.js
+++ src/components/Home/Dashboard.js
@@ -1,13 +1,34 @@
 import React from "react";
 import DashboardBackground from "../../assets/dashboard.png";
+import DashboardImage from "../../assets/dashboardimage.png";
+import { ReactComponent as ArrowRight } from "../../assets/arrowRight.svg";
 
 function Dashboard() {
   return (
     <div
-      className="mt-48"
+      className="mt-48  grid grid-cols-2 py-24 gap-8"
       style={{ backgroundImage: `url(${DashboardBackground})` }}
     >
-      G
+      <img src={DashboardImage} alt="dashboard" />
+      <div className=" flex flex-col justify-center">
+        <h3 className="font-medium text-[#145858]">Dashboard</h3>
+        <h2 className="font-grotesk font-bold text-4xl text-[#0F1111] w-[450px]">
+          Track all your team progress from one place.
+        </h2>
+        <p className="mt-6 mb-10 w-[540px] text-[#1E2121]">
+          See how you are doing in real time. Help everyone understand how they
+          are performing against their assigned tasks and find room for
+          improvement. Say goodbye to messy and time consuming reporting. With
+          ProjectMaster,get real time intelligence instantly on all your
+          projects anytime, anywhere.
+        </p>
+        <h2 className="inline-flex items-center font-medium text-[#145858]">
+          Get Started
+          <span className="ml-3 hover:rotate-45">
+            <ArrowRight />
+          </span>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -5,34 +5,32 @@ import { ReactComponent as Tiles } from "../../assets/tiles.svg";
 
 function Hero() {
   return (
-    <>
-      <div className="bg-[#FDF0D1] flex flex-col items-center">
-        <h3 className="font-grotesk font-bold text-6xl pt-14 w-[800px] leading-[72px]">
-          Better <span className="text-green">Collaborations</span>. Build
-          master projects together.{" "}
-          <span className="inline-flex">
-            <Lightning />
+    <div className="bg-[#FDF0D1] flex flex-col items-center">
+      <h3 className="font-grotesk font-bold text-6xl pt-14 w-[800px] leading-[72px]">
+        Better <span className="text-green">Collaborations</span>. Build master
+        projects together.{" "}
+        <span className="inline-flex">
+          <Lightning />
+        </span>
+      </h3>
+      <h4 className="text-[#1E2121] text-2xl my-6 w-[800px] text-center">
+        Increase your team’s productivity, meet your project and product
+        goals,exceed them and build worldclass products. Collaborate, manage
+        your projects. Build it out with ProjectMaster.
+      </h4>
+      <div>
+        <button className="bg-green text-white hover:bg-grey hover:text-green font-medium py-4 px-10 rounded-lg">
+          Get Started for free
+        </button>
+        <h2 className="inline-flex items-center font-medium text-[#145858] ml-6">
+          Learn More
+          <span className="ml-3 hover:rotate-45">
+            <ArrowRight />
           </span>
-        </h3>
-        <h4 className="text-[#1E2121] text-2xl my-6 w-[800px] text-center">
-          Increase your team’s productivity, meet your project and product
-          goals,exceed them and build worldclass products. Collaborate, manage
-          your projects. Build it out with ProjectMaster.
-        </h4>
-        <div>
-          <button className="bg-green text-white hover:bg-grey hover:text-green font-medium py-4 px-10 rounded-lg">
-            Get Started for free
-          </button>
-          <h2 className="inline-flex items-center ml-6">
-            Learn More
-            <span className="ml-3">
-              <ArrowRight />
-            </span>
-          </h2>
-        </div>
-        <Tiles />
+        </h2>
       </div>
-    </>
+      <Tiles />
+    </div>
   );
 }
 
