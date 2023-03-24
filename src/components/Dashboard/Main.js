799408f3e576a50e428f97a89ff98ff08b538e05
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2023-03-24 16:47:47 +0100

    feat: Navbar to dashboard

--- src/components/Dashboard/Main.js
+++ src/components/Dashboard/Main.js
@@ -22,6 +22,7 @@ function Main() {
               <PrintCard />
             </div>
           </div>
+          <PrintCard />
         </div>
         <div className="col-span-3">Tough lid=fe</div>
       </div>
